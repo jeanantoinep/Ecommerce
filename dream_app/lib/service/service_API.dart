f3a589003e399fb6847552b23d8071144ee25d86
djibrill <sow_d@etna-alternance.net>
2022-06-15 01:16:00 +0200

    add:init

---
+++ dream_app/lib/service/service_API.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+
+  }
+}
